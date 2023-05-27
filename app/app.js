359c5ec98886a6c54b4cd08f6717ea811e255026
Hasan Hafizur Rahman <hasanshanto922@gmail.com>
2023-05-27 17:13:56 +0600

    client server error handling

---
+++ app/app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const isLoggedIn = (_, res, next) => {
+  console.log("middleware logged");
+  next();
+};
+
+app.get("/", (_, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/api/user", isLoggedIn, (_, res) => {
+  res.status(200).send({
+    message: "user profile is returned",
+  });
+});
+
+app.get("/test", (_, res) => {
+  res.send("Testing successfull!");
+});
+
+// client error handler
+app.use((_, res, next) => {
+  res.status(404).send({ message: "User not found" });
+  next();
+});
+
+// server error handler
+app.use((err, res, next) => {
+  console.log(err);
+  res.status(500).send({ message: "Server error" });
+});
+
+module.exports = app;
--- app/server.js
+++ app/server.js
@@ -1,10 +1,5 @@
-const express = require("express");
-const app = express();
+const app = require("./app");
 const port = 5000;
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
